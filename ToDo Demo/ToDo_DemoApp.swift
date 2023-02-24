e8a46e09a8d0aa919d267aa0a453c2d4d3ccb1b8
Israa Kamel <israa.almarzooqi-1@hotmail.com>
2023-02-24 10:13:23 +0400

    Initial Commit

---
+++ ToDo Demo/ToDo_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_DemoApp.swift
+//  ToDo Demo
+//
+//  Created by apple on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
